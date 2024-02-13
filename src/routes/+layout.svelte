32caa8e789d29fd93a3922e7e6d55de16a375a7c
Zasda Yusuf Mikail <zasdaym@gmail.com>
2024-02-13 11:39:16 +0700

    Disable analytics on dev

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,21 @@
+<script lang="ts">
+  import { dev } from "$app/environment";
+  import {
+    PUBLIC_ANALYTICS_URL,
+    PUBLIC_ANALYTICS_ID,
+  } from "$env/static/public";
+</script>
+
+<svelte:head>
+  {#if !dev}
+    <script
+      async
+      src={PUBLIC_ANALYTICS_URL}
+      data-website-id={PUBLIC_ANALYTICS_ID}
+    ></script>
+  {/if}
+</svelte:head>
+
 <main>
   <slot />
 </main>
