e349b8d138adc41a77c432c224a56491ca786329
Zasda Yusuf Mikail <zasdaym@gmail.com>
2024-03-23 23:46:18 +0700

    Use pico.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,20 @@
 <script lang="ts">
-  import { dev } from "$app/environment";
-  import {
-    PUBLIC_ANALYTICS_URL,
-    PUBLIC_ANALYTICS_ID,
-  } from "$env/static/public";
+    import { dev } from "$app/environment";
+    import {
+        PUBLIC_ANALYTICS_ID,
+        PUBLIC_ANALYTICS_URL,
+    } from "$env/static/public";
+    import "../app.scss";
 </script>
 
 <svelte:head>
-  {#if !dev}
-    <script
-      async
-      src={PUBLIC_ANALYTICS_URL}
-      data-website-id={PUBLIC_ANALYTICS_ID}
-    ></script>
-  {/if}
+    {#if !dev}
+        <script
+            async
+            src={PUBLIC_ANALYTICS_URL}
+            data-website-id={PUBLIC_ANALYTICS_ID}
+        ></script>
+    {/if}
 </svelte:head>
 
-<main>
-  <slot />
-</main>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,119 +34,127 @@
     />
 </svelte:head>
 
-<h1>Kalkulator PPh 21 dengan Tarif Efektif Rata-rata (TER)</h1>
-<label for="salary">Gaji bulanan</label>
-<input
-    id="salary"
-    type="number"
-    bind:value={salary}
-    min="0"
-    placeholder="10000000"
-/>
-<label for="bonus">Bonus tahunan (THR, Jasa produksi, dll)</label>
-<input
-    id="bonus"
-    type="number"
-    bind:value={bonus}
-    min="0"
-    placeholder="10000000"
-/>
-<label for="status">Status PTKP</label>
-<select id="status" bind:value={status}>
-    {#each taxpayerStatuses as taxpayerStatus}
-        <option value={taxpayerStatus}>{taxpayerStatus}</option>
-    {/each}
-</select>
-<button
-    data-umami-event="calculate-salary"
-    data-umami-event-salary={salary}
-    data-umami-event-bonus={bonus}
-    data-umami-event-status={status}
-    on:click={onClick}
->
-    Hitung
-</button>
-<table>
-    <thead>
-        <tr>
-            <th>Komponen</th>
-            <th>Nominal</th>
-        </tr>
-    </thead>
-    <tr>
-        <td>Gaji Bulanan</td>
-        <td>{formatIdr(salary)}</td>
-    </tr>
-    <tr>
-        <td>Bonus</td>
-        <td>{formatIdr(bonus)}</td>
-    </tr>
-    <tr>
-        <td>BPJSTK JKK - Perusahaan (0,24%)</td>
-        <td>{formatIdr(result.employerContribution.jkk)}</td>
-    </tr>
-    <tr>
-        <td>BPJSTK JKM - Perusahaan (0,3%)</td>
-        <td>{formatIdr(result.employerContribution.jkm)}</td>
-    </tr>
-    <tr>
-        <td>BPJSKES - Perusahaan (4%, maks. Rp480.000)</td>
-        <td>{formatIdr(result.employerContribution.bpjskes)}</td>
-    </tr>
-    <tr>
-        <td>BPJSTK JHT - Karyawan (2%)</td>
-        <td>- {formatIdr(result.employeeContribution.jht)}</td>
-    </tr>
-    <tr>
-        <td>BPJSTK JP - Karyawan (1%, maks. Rp95.596)</td>
-        <td>- {formatIdr(result.employeeContribution.jp)}</td>
-    </tr>
-    <tr>
-        <td>Biaya Jabatan (5%, maks. Rp.500.000)</td>
-        <td>- {formatIdr(result.occupationalExpense)}</td>
-    </tr>
-    <tr>
-        <td>Penghasilan Bruto Bulanan</td>
-        <td>{formatIdr(result.monthlyGrossIncome)}</td>
-    </tr>
-    <tr>
-        <td>Penghasilan Bruto Tahunan</td>
-        <td>{formatIdr(result.yearlyGrossIncome)}</td>
-    </tr>
-    <tr>
-        <td>Penghasilan Neto Tahunan</td>
-        <td>{formatIdr(result.yearlyNetIncome)}</td>
-    </tr>
-    <tr>
-        <td>Penghasilan Tidak Kena Pajak</td>
-        <td>- {formatIdr(result.nonTaxableIncome)}</td>
-    </tr>
-    <tr>
-        <td>Penghasilan Kena Pajak</td>
-        <td>{formatIdr(result.taxableIncome)}</td>
-    </tr>
-    <tr>
-        <td>Pajak Bulanan</td>
-        <td>{formatIdr(result.regularMonthTax)}</td>
-    </tr>
-    <tr>
-        <td>Pajak di Bulan Bonus</td>
-        <td>{formatIdr(result.bonusMonthTax)}</td>
-    </tr>
-    <tr>
-        <td>Pajak di Bulan Desember</td>
-        <td>{formatIdr(result.decemberMonthTax)}</td>
-    </tr>
-    <tr>
-        <td>Total Pajak Setahun</td>
-        <td>{formatIdr(result.totalTax)}</td>
-    </tr>
-    <tr>
-        <td>Take Home Pay Bulanan</td>
-        <td>{formatIdr(result.regularMonthTakeHomePay)}</td>
-    </tr>
-    <tr>
-        <td>Take Home Pay di Bulan bonus</td>
-        <td>{formatIdr(result.bonusMonthTakeHomePay)}</td>
-    </tr>
-</table>
+<header>
+    <h1>Kalkulator PPh 21 dengan Tarif Efektif Rata-rata (TER)</h1>
+</header>
+<main>
+    <section>
+        <label for="salary">Gaji bulanan</label>
+        <input
+            id="salary"
+            type="number"
+            bind:value={salary}
+            min="0"
+            placeholder="10000000"
+        />
+        <label for="bonus">Bonus tahunan (THR, Jasa produksi, dll)</label>
+        <input
+            id="bonus"
+            type="number"
+            bind:value={bonus}
+            min="0"
+            placeholder="10000000"
+        />
+        <label for="status">Status PTKP</label>
+        <select id="status" bind:value={status}>
+            {#each taxpayerStatuses as taxpayerStatus}
+                <option value={taxpayerStatus}>{taxpayerStatus}</option>
+            {/each}
+        </select>
+        <button
+            data-umami-event="calculate-salary"
+            data-umami-event-salary={salary}
+            data-umami-event-bonus={bonus}
+            data-umami-event-status={status}
+            on:click={onClick}
+        >
+            Hitung
+        </button>
+    </section>
+    <section>
+        <table>
+            <thead>
+                <tr>
+                    <th>Komponen</th>
+                    <th>Nominal</th>
+                </tr>
+            </thead>
+            <tr>
+                <td>Gaji Bulanan</td>
+                <td>{formatIdr(salary)}</td>
+            </tr>
+            <tr>
+                <td>Bonus</td>
+                <td>{formatIdr(bonus)}</td>
+            </tr>
+            <tr>
+                <td>BPJSTK JKK - Perusahaan (0,24%)</td>
+                <td>{formatIdr(result.employerContribution.jkk)}</td>
+            </tr>
+            <tr>
+                <td>BPJSTK JKM - Perusahaan (0,3%)</td>
+                <td>{formatIdr(result.employerContribution.jkm)}</td>
+            </tr>
+            <tr>
+                <td>BPJSKES - Perusahaan (4%, maks. Rp480.000)</td>
+                <td>{formatIdr(result.employerContribution.bpjskes)}</td>
+            </tr>
+            <tr>
+                <td>BPJSTK JHT - Karyawan (2%)</td>
+                <td>- {formatIdr(result.employeeContribution.jht)}</td>
+            </tr>
+            <tr>
+                <td>BPJSTK JP - Karyawan (1%, maks. Rp95.596)</td>
+                <td>- {formatIdr(result.employeeContribution.jp)}</td>
+            </tr>
+            <tr>
+                <td>Biaya Jabatan (5%, maks. Rp.500.000)</td>
+                <td>- {formatIdr(result.occupationalExpense)}</td>
+            </tr>
+            <tr>
+                <td>Penghasilan Bruto Bulanan</td>
+                <td>{formatIdr(result.monthlyGrossIncome)}</td>
+            </tr>
+            <tr>
+                <td>Penghasilan Bruto Tahunan</td>
+                <td>{formatIdr(result.yearlyGrossIncome)}</td>
+            </tr>
+            <tr>
+                <td>Penghasilan Neto Tahunan</td>
+                <td>{formatIdr(result.yearlyNetIncome)}</td>
+            </tr>
+            <tr>
+                <td>Penghasilan Tidak Kena Pajak</td>
+                <td>- {formatIdr(result.nonTaxableIncome)}</td>
+            </tr>
+            <tr>
+                <td>Penghasilan Kena Pajak</td>
+                <td>{formatIdr(result.taxableIncome)}</td>
+            </tr>
+            <tr>
+                <td>Pajak Bulanan</td>
+                <td>{formatIdr(result.regularMonthTax)}</td>
+            </tr>
+            <tr>
+                <td>Pajak di Bulan Bonus</td>
+                <td>{formatIdr(result.bonusMonthTax)}</td>
+            </tr>
+            <tr>
+                <td>Pajak di Bulan Desember</td>
+                <td>{formatIdr(result.decemberMonthTax)}</td>
+            </tr>
+            <tr>
+                <td>Total Pajak Setahun</td>
+                <td>{formatIdr(result.totalTax)}</td>
+            </tr>
+            <tr>
+                <td>Take Home Pay Bulanan</td>
+                <td>{formatIdr(result.regularMonthTakeHomePay)}</td>
+            </tr>
+            <tr>
+                <td>Take Home Pay di Bulan bonus</td>
+                <td>{formatIdr(result.bonusMonthTakeHomePay)}</td>
+            </tr>
+        </table>
+    </section>
+</main>
